5c9a8ff7c822a4a415e0e008255204efebaac970
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-05-07 18:42:38 +0900

    Add Clickable address to Google maps

--- codecademy/Ravenous/src/components/Business/Business.js
+++ codecademy/Ravenous/src/components/Business/Business.js
@@ -11,11 +11,11 @@ class Business extends React.Component {
               </div>
               <h2>{this.props.business.name}</h2>
               <div className="Business-information">
-                <div className="Business-address">
+                <a href={`https://www.google.com/maps/place/${this.props.business.address} ${this.props.business.name}`} className="Business-address">
                   <p>{this.props.business.address}</p>
                   <p>{this.props.business.city}</p>
                   <p>{this.props.business.state} {this.props.business.zipCode}</p>
-                </div>
+                </a>
                 <div className="Business-reviews">
                   <h3>{this.props.business.category}</h3>
                   <h3 className="rating">{this.props.business.rating} stars</h3>
