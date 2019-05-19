1e0054420cdda09c7ca1db6c4ab2b2f5feffe508
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-05-20 02:21:53 +0900

    add Pass down search result and Render result list

--- codecademy/Jammming/src/Components/App/App.js
+++ codecademy/Jammming/src/Components/App/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './App.css';
+import Playlist from "../components/Playlist.js"
+import SearchBar from "../components/SearchBar.js"
+import SearchResults from "../components/SearchResults.js"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      searchResults: {
+        name: "some city pop",
+        artist: "some artist",
+        album: "some albun",
+        id: "001"
+      }
+    }
+  }
+
+
   render() {
     return (
       <div>
@@ -9,7 +26,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults} />
             <Playlist />
           </div>
         </div>
