945bf7cc9399ee34d53113ba008105bf1dfe95c8
SeiyaIskw <SeiyaIskw@users.noreply.github.com>
2019-05-22 11:31:28 +0900

    Changed Eventhandler on SearchBar & Add Search when Enterkey Pressed method

--- codecademy/Jammming/src/Components/SearchBar/SearchBar.js
+++ codecademy/Jammming/src/Components/SearchBar/SearchBar.js
@@ -6,23 +6,34 @@ class SearchBar extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            term: ""
+        }
+
         this.search = this.search.bind(this)
         this.handleTermChange = this.handleTermChange.bind(this)
     }
 
     search(term) {
-        this.props.onSearch(term)
+        this.props.onSearch(this.state.term)
     }
 
     handleTermChange(event) {
-        this.search(event.target.value)
+        this.setState({ term: event.target.value })
+    }
+
+    // Search when EnterKey Pressed
+    handleEnterKey(event) {
+        if (event.keyCode === 13) {
+            this.search(this.state.term)
+        }
     }
 
     render() {
         return (
             <div className="SearchBar">
-              <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
-              <button className="SearchButton">SEARCH</button>
+              <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleEnterKey} />
+              <button className="SearchButton" onClick={this.search}>SEARCH</button>
             </div>
         )
     }
